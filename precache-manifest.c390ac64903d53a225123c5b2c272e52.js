58b8cd568bee71a3dbd17b373ee806d53651772d
popy01012 <popy01012@gmail.com>
2021-03-30 00:08:20 +0900

    Updates

---
+++ precache-manifest.c390ac64903d53a225123c5b2c272e52.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57f661b8d807f26c642336a17b8ee9c4",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "5236a0c20809ebed37f2",
+    "url": "/Portfolio/static/css/main.792e228d.chunk.css"
+  },
+  {
+    "revision": "62d43565964871220643",
+    "url": "/Portfolio/static/js/2.75c486b5.chunk.js"
+  },
+  {
+    "revision": "345c489e7d9a6974e7fc52acc51f4c14",
+    "url": "/Portfolio/static/js/2.75c486b5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5236a0c20809ebed37f2",
+    "url": "/Portfolio/static/js/main.42fab90d.chunk.js"
+  },
+  {
+    "revision": "2fa92be0e95ca7495fd3",
+    "url": "/Portfolio/static/js/runtime-main.65bb930e.js"
+  },
+  {
+    "revision": "b88924d5d914bf9f3885f87c2ed76477",
+    "url": "/Portfolio/static/media/sad-icon.b88924d5.png"
+  },
+  {
+    "revision": "b9d7d448a09d6aacb45d21aab2c6fd53",
+    "url": "/Portfolio/static/media/your photo.b9d7d448.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/ReactFolio/precache-manifest.45f920b73778e941d63035d45c79f139.js"
+  "/Portfolio/precache-manifest.c390ac64903d53a225123c5b2c272e52.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/ReactFolio/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Portfolio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
